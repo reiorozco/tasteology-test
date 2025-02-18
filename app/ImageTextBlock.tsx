e508cd01217c6627601688c9b037363ba996bb51
reiorozco <rfoc15@gmail.com>
2025-02-17 19:58:21 -0500

    Add images assets Improve layout

--- app/ImageTextBlock.tsx
+++ app/ImageTextBlock.tsx
@@ -1,16 +1,23 @@
 import React from "react";
+import Image from "next/image";
 import { Box, Grid } from "@radix-ui/themes";
 import ImageTextContent from "@/app/ImageTextContent";
 
 function ImageTextBlock() {
   return (
-    <Grid columns="10" className="p-10 size-full" gap="4">
+    <Grid columns="10" className="p-10" gap="4">
       <Grid columns="2" rows="2" className="col-span-7" gap="2">
-        <Box className="red-border row-span-2 size-full">Image 1</Box>
+        <Box className="row-span-2" position="relative">
+          <Image src="/images/left-image.png" alt="recipe image" fill />
+        </Box>
 
-        <Box className="red-border size-full">Image 2</Box>
+        <Box position="relative">
+          <Image src="/images/right-top-image.png" alt="recipe image" fill />
+        </Box>
 
-        <Box className="red-border size-full">Image 3</Box>
+        <Box position="relative">
+          <Image src="/images/right-bottom-image.png" alt="recipe image" fill />
+        </Box>
       </Grid>
 
       <ImageTextContent />
