1ba781a030a604cada2e905222809da90d270ad8
reiorozco <rfoc15@gmail.com>
2025-02-18 10:20:09 -0500

    Add opacity hover effect

--- app/components/Card.tsx
+++ app/components/Card.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Image from "next/image";
+import Link from "next/link";
 import { Box, Flex, Heading, Text } from "@radix-ui/themes";
 
 interface Props {
@@ -11,7 +12,20 @@ interface Props {
 function Card({ title, content, imageSrc }: Props) {
   return (
     <Flex direction="column" align="center" gap="5">
-      <Image src={imageSrc} alt="card image" width="375" height="300" />
+      <Box className="w-full relative max-w-sm mx-auto h-auto ">
+        <Image
+          src={imageSrc}
+          alt="card image"
+          width="375"
+          height="300"
+          className="w-full max-w-sm mx-auto h-auto relative z-0"
+        />
+
+        <Link
+          href="#"
+          className="absolute w-full h-full top-0 left-0 bg-white opacity-0 z-10 transition-opacity duration-300 hover:opacity-15"
+        />
+      </Box>
 
       <Box>
         <Heading size="4" mb="1" weight="medium" align="center">
